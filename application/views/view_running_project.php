328994a31e1d5da8596fc4a647c97330c4969b7a
LLC <tube.barron@gmail.com>
2018-01-08 18:41:16 +0600

    Update view running project page

--- application/views/view_running_project.php
+++ application/views/view_running_project.php
@@ -52,9 +52,12 @@ include('admin_sidebar.php');
                             <td>
                               <a data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-info btn-sm" href="<?php echo base_url();?>Add_Project_Controller/Edit_Current_Project/<?php echo $results->Id;?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
 </a>
-                              <a class="btn btn-outline-danger " href="<?php echo base_url();?>Add_Project_Controller/Close_Current_Project/<?php echo $results->Id;?>" onclick="ConfirmClose()">Close</a>
-                               <a class="btn btn-outline-dark margin-top" href="<?php echo base_url();?>Add_Project_Controller/Withdraw_Expense_From_Current_Project/<?php echo $results->Id;?>">Expense</a>
-                                <a class="btn btn-outline-secondary margin-top" href="<?php echo base_url(); ?>Add_Project_Controller/View_Expense_List_Of_A_Project?pro_id=<?php echo $results->Id;?>">Expense History</a>
+                              <a data-toggle="tooltip" data-placement="top" title="Close" class="btn btn-danger btn-sm" href="<?php echo base_url();?>Add_Project_Controller/Close_Current_Project/<?php echo $results->Id;?>" onclick="ConfirmClose()"><i class="fa fa-times-circle" aria-hidden="true"></i>
+</a>
+                               <a data-toggle="tooltip" data-placement="top" title="Expense" class="btn btn-dark btn-sm"  href="<?php echo base_url();?>Add_Project_Controller/Withdraw_Expense_From_Current_Project/<?php echo $results->Id;?>"><i class="fa fa-eur" aria-hidden="true"></i>
+</a>
+                                <a data-toggle="tooltip" data-placement="top" title="Expense History" class="btn btn-success btn-sm margin-top" href="<?php echo base_url(); ?>Add_Project_Controller/View_Expense_List_Of_A_Project?pro_id=<?php echo $results->Id;?>"><i class="fa fa-history" aria-hidden="true"></i>
+</a>
  
                             </td>
                           <?php }
